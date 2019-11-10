101584c807b884505dd7eb8cd134ba45c640b76c
Ethan Hathaway <erhathaway@gmail.com>
2019-11-10 10:56:20 -0800

    Chore: add latest changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
-import Router from 'recursive-router-react';
+import routerGenerator from 'recursive-router-react';
 import logo from './logo.svg';
 import './App.css';
+
+const Router = routerGenerator();
+
+class DelayRender extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false,
+    }
+    console.log(this)
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ show: true }), this.props.delayMS);
+  }
+
+  render() {
+    if (this.state.show) return this.props.children
+    return null;
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
+      <div className="App">
+        <div> hi </div>
+        <Router name="root" type="root">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <Router name="home" type="scene" parent="root">
+              <img src={logo} className="App-logo" alt="logo" />
+              <Router name="home-data1" type="feature" parent="home"/>
+              <Router name="home-data2" type="feature" parent="home">
+                <DelayRender delayMS={200}>
+                  <Router name="home-scene1" type="scene" parent="home-data2"/>
+                </DelayRender>
+                <Router name="home-scene2" type="scene" parent="home-data2"/>
+              </Router>
+              <Router name="home-data3" type="feature" parent="home"/>
+            </Router>
+            <Router name="users" type="scene" parent="root">
+              <img src={logo} className="App-logo" alt="logo" />
+              <Router name="user-data1" type="data" parent="users"/>
+            </Router>
             <p>
               Edit <code>src/App.js</code> and save to reload.
             </p>
@@ -22,8 +58,8 @@ class App extends Component {
               Learn React
             </a>
           </header>
-        </div>
       </Router>
+        </div>
     );
   }
 }
